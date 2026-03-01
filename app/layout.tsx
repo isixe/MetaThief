1e17987ca32fbfbc460248a40f0bf4cb6dcc8f23
isixe <isixe@outlook.com>
2026-03-01 17:04:37 +0800

    ♻️ refactor(layout): Simplify analytics script handling
    - Replace conditional expressions with clearer logic for analyticsScript rendering.

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,19 +37,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const analyticsScript = process.env.ANALYTICS_SCRIPT ?? ''
+  const analyticsScript = process.env.ANALYTICS_SCRIPT || ''
 
   return (
     <html lang="en">
       <head>
-        {analyticsScript && (
+        {analyticsScript ? (
           <Script
             src={analyticsScript}
             id="analytics"
             data-website-id="metathief"
             defer
           />
-        )}
+        ) : null}
         <link rel="icon" href="/favicon.ico" />
       </head>
 
